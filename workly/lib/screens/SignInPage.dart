ad4e31fdb47e5fcb61551fee033f43aa7b2efe26
HX <huaixing1234@gmail.com>
2020-05-21 09:25:58 +0800

    SignInPage: Added a button

--- workly/lib/screens/SignInPage.dart
+++ workly/lib/screens/SignInPage.dart
@@ -10,31 +10,38 @@ class SignInPage extends StatelessWidget {
         elevation: 10.0, //shadow of appBar, default is 4.0
       ),
       body: _bodyContent(),
+      backgroundColor: Colors.grey[200], //colors to be decided
     );
   }
 }
 
 Widget _bodyContent() {
   return Padding(
-    padding: EdgeInsets.all(35.0),
+    padding: EdgeInsets.all(45.0),
     child: Column(
       mainAxisAlignment: MainAxisAlignment.center, //similar to y-axis
       crossAxisAlignment: CrossAxisAlignment.stretch, //similar to x-axis
       children: <Widget>[
-        Text('Sign in',
+        Text(
+          'Sign in',
           textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 28.0,
-            fontWeight: FontWeight.bold
-          ),
+          style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
         ),
         SizedBox(height: 50.0), //Separation between above text and below box
-        Container(
-          color: Colors.orange,
-          child: SizedBox(
-            height: 75.0,
+        RaisedButton(
+          child: Text(
+            "Sign in with email",
+            style: TextStyle(fontSize: 16.0),
           ),
-        ),
+          color: Colors.tealAccent[100], //Colours to be decided
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
+          ),
+          onPressed: () {},
+        )
       ],
     ),
   );
