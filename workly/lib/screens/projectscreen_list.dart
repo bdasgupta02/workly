c08bed3e673ee48e71b52b8b5e99e1c3263a1fb9
Bikram <bdasgupta@gmail.com>
2020-05-30 21:23:21 +0800

    Gesture controls to project screen

--- workly/lib/screens/projectscreen_list.dart
+++ workly/lib/screens/projectscreen_list.dart
@@ -22,6 +22,7 @@ class _AllProjectsState extends State<AllProjects> {
   }
 }
 
+//[Note] Constructs the list of widgets based on project objects by calling the construct() method
 class ListContructor {
   static ListView test() {
     List<Project> projectList = [
@@ -45,8 +46,7 @@ class ListContructor {
       ),
       Project(
         name: 'This is a massive title consisting of multiple lines lulz',
-        desc:
-            'The description is supposed to be long too!',
+        desc: 'The description is supposed to be long too!',
         deadline: '20/06/2020',
       ),
     ];
@@ -58,7 +58,9 @@ class ListContructor {
     for (int i = 0; i < projectList.length; i++) {
       widgetList.add(projectList[i].makeWidgetTile());
     }
-    return ListView(children: widgetList,);
+    return ListView(
+      children: widgetList,
+    );
   }
 }
 
@@ -67,24 +69,15 @@ class Project {
   String name;
   String desc;
   String deadline;
-  //list of tasks, ideas etc??
-
   Project({this.name, this.desc, this.deadline});
 
-  String getName() {
-    return name;
-  }
-
-  String getDesc() {
-    return desc;
-  }
-
-  String getDeadline() {
-    return deadline;
-  }
-
+  //[Note] This generates a SINGLE widget tile based on the project object attributes.
+  // If an object is not used, can just use those 3 strings in any way possible.
   Widget makeWidgetTile() {
     String newDesc = desc.length > 65 ? desc.substring(0, 65) + ' ...' : desc;
+
+    //[Placeholder] To navigate to project sub screens
+    Function _goToSubScreen = () => null;
 
     return Container(
       margin: EdgeInsets.only(right: 10, left: 10, bottom: 12),
@@ -109,83 +102,92 @@ class Project {
                 color: Color(0xFFFCFCFC),
                 borderRadius: BorderRadius.all(Radius.circular(30)),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: 15,
-                      right: 10,
-                      left: 17,
-                      bottom: 3,
-                    ),
-                    child: Text(
-                      name,
-                      style: TextStyle(
-                        color: Color(0xFF141336),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 19,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                onPressed: _goToSubScreen,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 15,
+                        right: 10,
+                        left: 0,
+                        bottom: 3,
+                      ),
+                      child: Text(
+                        name,
+                        style: TextStyle(
+                          color: Color(0xFF141336),
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 19,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: 3,
-                      right: 10,
-                      left: 17,
-                      bottom: 15,
-                    ),
-                    child: Text(
-                      newDesc,
-                      style: TextStyle(
-                        color: Colors.black45,
-                        fontFamily: 'Roboto',
-                        fontSize: 15,
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 3,
+                        right: 10,
+                        left: 0,
+                        bottom: 15,
+                      ),
+                      child: Text(
+                        newDesc,
+                        style: TextStyle(
+                          color: Colors.black45,
+                          fontFamily: 'Roboto',
+                          fontSize: 15,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
           Expanded(
             flex: 2,
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: 15,
-                      right: 10,
-                      bottom: 3,
-                    ),
-                    child: Text(
-                      'Deadline',
-                      style: TextStyle(
-                        color: Color(0xFFFCFCFC),
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w600,
-                        fontSize: 15,
+            child: GestureDetector(
+              onTap: _goToSubScreen,
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 15,
+                        right: 10,
+                        bottom: 3,
+                      ),
+                      child: Text(
+                        'Deadline',
+                        style: TextStyle(
+                          color: Color(0xFFFCFCFC),
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: 3,
-                      right: 10,
-                      bottom: 15,
-                    ),
-                    child: Text(
-                      deadline,
-                      style: TextStyle(
-                        color: Color(0xFFFCFCFC),
-                        fontFamily: 'Roboto',
-                        fontSize: 12,
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 3,
+                        right: 10,
+                        bottom: 15,
+                      ),
+                      child: Text(
+                        deadline,
+                        style: TextStyle(
+                          color: Color(0xFFFCFCFC),
+                          fontFamily: 'Roboto',
+                          fontSize: 12,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
